b9170b5f0f92a96ad37c0e3bd7c54562fee9fc49
Yuumei <shjesna@protonmail.com>
2026-04-29 12:12:35 +0200

    Scaffolding parser

---
+++ fumi/parser.h
@@ -0,0 +1,51 @@
+#include <mcu/core.h>
+#include <mcu/containers.h>
+
+typedef enum {
+   ANT_Module,
+   ANT_Procedure,
+   
+   ANT_VariableDecl,
+
+   ANT_IntLiteral
+} AstNodeType;
+
+/// AstNodeIndex
+typedef isize ANI;
+
+typedef struct {
+   AstNodeType type;
+
+   union {
+      struct {
+         String path;
+         Vector ANI_functions;
+      } module;
+
+      struct {
+         String name;
+         String return_type;
+         Vector ANI_body;
+      } procedure;
+
+      struct {
+         String name;
+         String type;
+         ANI expression;
+      } variable_decl;
+
+      i64 int_literal;
+   };
+} AstNode;
+
+typedef struct {
+   Vector AstNode_modules;
+   Vector AstNodes;
+} Ast;
+
+/// [failure] is allowed to be null.
+Ast Ast_parse_from_path(const cstr path, bool* failure);
+void Ast_free(Ast* self);
+
+void Ast_print(Ast* self);
+
